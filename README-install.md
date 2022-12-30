85ecbaf665628c4b3ae2d14d7710889c9825d47b
Roger-Franco <rogerfrancogalt@gmail.com>
2022-12-30 13:04:44 -0400

    finalização do contador

---
+++ README-install.md
@@ -0,0 +1,8 @@
+npm create vite@latest
+npm i react-router-dom 
+
+npm run dev
+
+https://www.youtube.com/watch?v=uYSV4w1FFnE&ab_channel=MatheusBattisti-HoradeCodar
+
+12:00 
--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
 import './App.css'
+import Counter from './components/Counter'
+import Title from './components/Title'
+
+import ImgBackGround from './assets/ferias.jpg'
+import useCountdown from './hooks/useCountdown'
 
 function App() {
+  const [day, hour, minute, second] = useCountdown("jan 1, 2023 00:00:00")
+  console.log(day);
 
   return (
-    <div className="App">
-      <h1>Countdown</h1>
+    <div className="App" style={{ backgroundImage: `url(${ImgBackGround})` }}>
+      <div className="container">
+        <Title title="Contagem regressiva para 2023" />
+        <div className="countdown-container">
+          <Counter title="Dias" number={day} />
+          <Counter title="Horas" number={hour} />
+          <Counter title="Minutos" number={minute} />
+          <Counter title="Segundos" number={second} />
+        </div>
+      </div>
     </div>
   )
 }
